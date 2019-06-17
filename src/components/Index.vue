559488210ca56a54e35ae0df085ae7b52ff0b06a
Assanali <asanmaris@gmail.com>
2019-06-17 17:11:10 +0600

    add index component (home page), add deleteRecipe method

---
+++ src/components/Index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="recipe in recipes" :key="recipe.id">
+      <div class="card-content">
+        <i class="material-icons delete" @click="deleteRecipe(recipe.id)">delete</i>
+        <h2 class="indigo-text">{{ recipe.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in recipe.ingredients" :key="index">
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      recipes: [
+        { title: 'Ninja bro', slug: 'ninja-brew', ingredients: ['bananas', 'cofee', 'milk'], id: '1' },
+        { title: 'Morning mood', slug: 'Morning Mood', ingredients: ['mango', 'line', 'juice'], id: '2' }
+      ]
+    }
+  },
+  methods: {
+    deleteRecipe(id) {
+      this.recipes = this.recipes.filter(recipe => {
+        return recipe.id != id
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+
+.index .ingredients {
+  margin: 30px auto;
+}
+
+.index .ingredients li {
+  display: inline-block;
+}
+
+.index .delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
+}
+</style>
